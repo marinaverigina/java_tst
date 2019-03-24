7ab29e565293132dbd489f8b81a184e0ab74f11d
Marina Verigina <vemarimi@gmail.com>
2019-03-24 16:31:53 +0300

    Внесли изменения в проект Sandbox

--- sandbox/src/main/java/ru/stqa/pft/sandbox/TestProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/TestProgram.java
@@ -1,7 +1,28 @@
 package ru.stqa.pft.sandbox;
 
-public class  TestProgram{
-	public static void main(String[] args) {
-		System.out.println("what the f*ck");			
-	}
+public class TestProgram {
+    public static void main(String[] args) {
+        hello("user");
+        hello("Marina");
+
+        double v = 10;
+        System.out.println("Площадь квадрата со стороной " + v + " равна " + area(v));
+
+        double a = 5;
+        double b = 4;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
+
+    }
+
+    public static void hello(String s) {
+        System.out.println("Hello, " + s);
+    }
+
+    public static double area(double x) {
+        return x * x;
+    }
+
+    public static double area(double a, double b){
+        return a*b;
+    }
 }
